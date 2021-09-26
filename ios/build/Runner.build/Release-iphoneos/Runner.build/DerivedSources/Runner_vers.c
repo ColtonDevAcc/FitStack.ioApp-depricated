cb95f5713a8df76e91b23ac6df4f832eab24e537
Colton Bristow <56052449+ColtonDevAcc@users.noreply.github.com>
2021-09-26 04:02:56 -0500

    ui fix plus xcode config changes

--- ios/build/Runner.build/Release-iphoneos/Runner.build/DerivedSources/Runner_vers.c
+++ ios/build/Runner.build/Release-iphoneos/Runner.build/DerivedSources/Runner_vers.c
@@ -1,5 +1,5 @@
  extern const unsigned char RunnerVersionString[];
  extern const double RunnerVersionNumber;
 
- const unsigned char RunnerVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Runner  PROJECT:Runner-3" "\n";
- const double RunnerVersionNumber __attribute__ ((used)) = (double)3.;
+ const unsigned char RunnerVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Runner  PROJECT:Runner-4" "\n";
+ const double RunnerVersionNumber __attribute__ ((used)) = (double)4.;
