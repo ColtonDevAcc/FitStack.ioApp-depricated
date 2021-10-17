9d3acd0b8f631637c992aaad8ea4a55698ab5241
Colton Bristow <56052449+ColtonDevAcc@users.noreply.github.com>
2021-10-17 00:06:17 -0500

    implementation of muscle selection on recovery view

---
+++ lib/controllers/recoveryModel_controller.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/parser.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:svg_path_parser/svg_path_parser.dart';
+import 'package:workify/models/recovery/recovery_model.dart';
+import 'package:workify/repositories/customExceptions.dart';
+
+final MuscleListControllerProvider =
+    StateNotifierProvider.family<MuscleListController, AsyncValue<List<Muscle>>, List<Muscle>>(
+        (ref, paths) {
+  return MuscleListController(ref.read, '', paths);
+});
+
+final groupListExceptionProvider = StateProvider<CustomException?>((_) => null);
+
+class MuscleListController extends StateNotifier<AsyncValue<List<Muscle>>> {
+  final Reader read;
+  final String? userId;
+  final List<Muscle> muscleList;
+
+  MuscleListController(this.read, this.userId, this.muscleList) : super(AsyncValue.loading()) {
+    userId == null ? muscleModelImageParser() : recoveryImageParser(this.muscleList);
+  }
+
+  recoveryImageParser(muscleList, {bool isRefreshing = false}) async {
+    if (isRefreshing) state = AsyncValue.loading();
+    try {
+      List<Muscle> parsedMuscleList = [];
+
+      for (Muscle muscle in muscleList) {
+        muscle.parsedPath = parseSvgPath(muscle.path);
+        parsedMuscleList.add(muscle);
+      }
+      if (mounted) {
+        state = AsyncValue.data(parsedMuscleList);
+      }
+      return parsedMuscleList;
+    } catch (e) {
+      state = AsyncValue.error(e);
+    }
+  }
+
+  muscleModelImageParser() {}
+}
+
+// final userProvider = ChangeNotifierProvider.autoDispose.family<UserProvider, bool>((ref, isFemale) {
+//   return UserProvider(isFemale);
+// });
--- lib/models/recovery/recovery_model.dart
+++ lib/models/recovery/recovery_model.dart
@@ -1,40 +1,68 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:flutter/foundation.dart';
+import 'package:flutter/cupertino.dart';
 
-part 'recovery_model.freezed.dart';
-part 'recovery_model.g.dart';
-
-@freezed
-abstract class RecoveryModel implements _$RecoveryModel {
-  const RecoveryModel._();
-
-  factory RecoveryModel({
-    String? id,
-    required String muscleGroup,
-    required String muscleName,
-    required String path,
-    required Color color,
-    required bool selectable,
-  }) = _RecoveryModel;
-
-  factory RecoveryModel.empty() => RecoveryModel(
-        muscleName: '',
-        muscleGroup: '',
-        path: '',
-        selectable: false,
-        color: Colors.black,
-      );
-
-  factory RecoveryModel.fromJson(Map<String, dynamic> json) => _$RecoveryModelFromJson(json);
-
-  factory RecoveryModel.fromDocument(DocumentSnapshot doc) {
-    final data = doc.data() as Map<String, dynamic>;
-    return RecoveryModel.fromJson(data).copyWith(id: doc.id);
-  }
-
-  Map<String, dynamic> toDocument() => toJson()..remove('id');
+enum PrimaryMuscleGroups {
+  //Primary Muscle Groups
+  empty,
+  vastusLateralist,
+  brachioradialis,
+  hand,
+  abdominals, //The muscles that make up the front of your mid-section.
+  biceps, //The muscles located on the front of your arm between your shoulder and elbow.
+  calves, //The muscles that make up the lower portion of the back of your leg below your knee.
+  deltoids, //The muscles that make up a large portion of the curve of your shoulder.
+  forearms, //The muscles located on the lower arm between the elbow and wrist.
+  gluteals, //The muscles of your posterior.
+  hamstrings, //The muscles located on the back of your leg between your gluteals and knee.
+  latissimusDorsi, //The muscles located on the sides of your upper back under the arm pit.
+  pectorals, //The muscles of your chest.
+  obliques, //The muscles on the sides of your mid-section.
+  quadriceps, //The muscles located on the front of your leg between your hip and knee.
+  trapezius, //The muscles located on the sides of your neck.
+  triceps, //The muscles located on the back of your arm between your shoulder and elbow.
 }
 
-//!flutter packages pub run build_runner watch --delete-conflicting-outputs
+enum PushMuscleGroups {
+  //The muscle groups listed below are considered push muscle groups.
+  //The intent of performing a push/pull weight training routine is to
+  //exercise one or more of the muscle groups listed below with one or
+  //more of the pull muscle groups.
+  calves,
+  deltoids,
+  gluteals,
+  pectorals,
+  quadriceps,
+  triceps,
+}
+
+enum PullMuscleGroups {
+  //The muscle groups listed below are considered pull muscle groups.
+  //When performing a push/pull weight training routine,
+  //the intent is to exercise one or more of the pull muscle groups
+  //listed below with one or more of the pull muscle groups listed above.
+  abdominals,
+  biceps,
+  forearms,
+  latissimusDorsi,
+  hamstring,
+  obliques,
+  trapezius,
+}
+
+class Muscle {
+  final String muscleName;
+  final PrimaryMuscleGroups muscleGroup;
+  final Color nonSelectedModelColor;
+  final Color selctedModelColor;
+  final String path;
+  Path? parsedPath;
+
+  List<PrimaryMuscleGroups>? selectedMuscleGroup;
+
+  Muscle(
+    this.muscleName,
+    this.muscleGroup,
+    this.nonSelectedModelColor,
+    this.selctedModelColor,
+    this.path,
+  );
+}
